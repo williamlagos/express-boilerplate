dfd797113b8fdb451ef5dc3d6bc72fab367e729f
William Oliveira de Lagos <william.lagos1@gmail.com>
2017-02-13 11:29:29 -0200

    Refreshed structure and dependencies for development

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var bcrypt = require('bcrypt');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -61,8 +60,6 @@ var userResource = epilogue.resource({
     endpoints:['/users','/users/:id']
 });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
