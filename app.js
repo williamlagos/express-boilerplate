8c6171497e45177349c681cdbe92a493a5f750f7
William Oliveira de Lagos <william.lagos@live.com>
2017-03-01 23:53:42 +0200

    Found bug in integration between sequelize and passport

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var epilogue = require('epilogue');
-var passport = require('passport');
-var Strategy = require('passport-http').BasicStrategy;
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
 
 var index = require('./routes/index');
 
@@ -62,27 +62,55 @@ var movieResource = epilogue.resource({
 });
 
 app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(express.session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use(app.router);
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({where: { name: username }}).then(function(user){
+      return done(null, user);
+    });
+  }
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* Code for Basic HTTP Authentication with Passport */
 app.use('/', index);
-passport.use(new Strategy(
-    function(username,password,done){
-        User.findOne({ username:username }, function(err,user){
-            if(err){ return done(err); }
-            if(!user){ return done(null,false); }
-            if(!user.validPassword(password)){
-                return done(null,false);
-            }
-            return done(null,user);
-        });
-    }
-));
-app.post('/me',
+
+app.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { return res.json({title:info}); }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.redirect('/users/' + user.username);
+    });
+  })(req, res, next);
+});
+
+/*app.get('/me',
     passport.authenticate('basic',{ session: false }),
-    function(req,res){ res.json(req.user); });
+    function(req,res){ res.json(req.user); });*/
+    /*passport.use(new Strategy(
+        function(username,password,done){
+            User.findOne({ username:username }, function(err,user){
+                if(err){ return done(err); }
+                if(!user){ return done(null,false); }
+                if(!user.validPassword(password)){
+                    return done(null,false);
+                }
+                return done(null,user);
+            });
+        }
+    ));*/
 
 module.exports = app;
 exports.database = database;
